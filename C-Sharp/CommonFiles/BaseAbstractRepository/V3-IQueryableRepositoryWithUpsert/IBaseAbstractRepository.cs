c1e8a2309bfae161ffd16f6ac6516597e3285577
christofer <chrisb66110@gmail.com>
2020-04-27 22:15:43 -0600

    Add Upsert and UpsertRange in BaseAbstractRepository

---
+++ C-Sharp/CommonFiles/BaseAbstractRepository/V3-IQueryableRepositoryWithUpsert/IBaseAbstractRepository.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Prueba.Dal.Repositories
+{
+    /*
+     * To use this respository, you need register the generic repository in start up, you can add:
+     *          builder.RegisterGeneric(typeof(BaseAbstractRepository<,,>)).As(typeof(IBaseAbstractRepository<,,>));
+     *
+     * To use in Bll, you need add dependency
+     *          IBaseAbstractRepository<model, modelDto, yourContext> modelRepository
+     */
+    public interface IBaseAbstractRepository<TEntity, TDto, TContext>
+        where TEntity : class
+        where TContext : DbContext
+    {
+        public Task<List<TDto>> GetAsync(Func<IQueryableRepository<TEntity>, IQueryableRepository<TEntity>> filterFunction = null);
+
+        public Task<List<TResultGruoupBy>> GetAsync<TResultGruoupBy>(Func<IQueryableRepository<TEntity>, IQueryableRepository<TResultGruoupBy>> filterFunction);
+
+        public Task<TDto> GetFirstOrDefaultAsync(Func<IQueryableRepository<TEntity>, IQueryableRepository<TEntity>> filterFunction = null);
+
+        public Task<TDto> AddAsync(TDto dto);
+
+        public Task<List<TDto>> AddRangeAsync(List<TDto> dtos);
+
+        public Task<TDto> UpdateAsync(TDto dto);
+
+        public Task<List<TDto>> UpdateRangeAsync(List<TDto> dto);
+
+        public Task<TDto> RemoveAsync(TDto dto);
+
+        public Task<List<TDto>> RemoveRangeAsync(List<TDto> dto);
+
+        public Task<TDto> UpsertAsync(TDto dto, Expression<Func<TEntity, object>> on, Expression<Func<TEntity, TEntity, TEntity>> whenMatched);
+
+        public Task<List<TDto>> UpsertRangeAsync(List<TDto> dto, Expression<Func<TEntity, object>> on, Expression<Func<TEntity, TEntity, TEntity>> whenMatched);
+    }
+}
---
+++ C-Sharp/CommonFiles/BaseAbstractRepository/V3-IQueryableRepositoryWithUpsert/QueryableAbstract.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Prueba.Dal.Repositories
+{
+    public class QueryableAbstract<T> : IQueryableRepository<T>
+    {
+        private IQueryable<T> _source;
+
+        public QueryableAbstract(IQueryable<T> source)
+        {
+            _source = source;
+        }
+
+        public static IQueryableRepository<T> AsQueryableRepository(IQueryable<T> source)
+        {
+            return new QueryableAbstract<T>(source);
+        }
+
+        public IQueryable<T> AsQueryable()
+        {
+            return this._source;
+        }
+
+        public IQueryableRepository<T> Where(Expression<Func<T, bool>> predicate)
+        {
+            _source = Queryable.Where<T>((IQueryable<T>) _source, predicate);
+            return this;
+        }
+
+        public IQueryableRepository<T> Select(Expression<Func<T, T>> selector)
+        {
+            _source = Queryable.Select<T,T>((IQueryable<T>) _source, selector);
+            return this;
+        }
+        
+        public IQueryableRepository<T> OrderBy<TKey>(Expression<Func<T, TKey>> keySelector)
+        {
+            _source = Queryable.OrderBy<T, TKey>((IQueryable<T>) _source, keySelector);
+            return this;
+        }
+
+        public IQueryableRepository<T> OrderByDescending<TKey>(Expression<Func<T, TKey>> keySelector)
+        {
+            _source = Queryable.OrderByDescending<T, TKey>((IQueryable<T>) _source, keySelector);
+            return this;
+        }
+
+
+        public IQueryableRepository<TResult> GroupBy<TKey, TResult>(Expression<Func<T, TKey>> keySelector, Expression<Func<TKey, IEnumerable<T>, TResult>> resultSelector)
+        {
+            var result = Queryable.GroupBy<T, TKey, TResult>((IQueryable<T>) _source, keySelector, resultSelector);
+            return new QueryableAbstract<TResult>(result);
+        }
+
+        public IQueryableRepository<T> Distinct()
+        {
+            _source = Queryable.Distinct<T>((IQueryable<T>) _source);
+            return this;
+        }
+    }
+}
