a2ad12001a896dc5f7fef410bbb90775283042f1
Christofer <chrisb66110@gmail.com>
2020-07-10 16:37:18 -0600

    Remove unnecessary code

--- C-Sharp/Catalogue/Catalogue/CatalogueMigrationScript.ps1
+++
@@ -1,7 +0,0 @@
-#Migrations Catalogue Project
-
-cd .\Source\Catalogue.Dal
-
-dotnet ef migrations add InitialMigration --context CatalogueContext --output-dir Migrations/Catalogue -v -s ..\Catalogue.Api\Catalogue.Api.csproj
-
-cd ..\..
